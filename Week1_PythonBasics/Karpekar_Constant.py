cecb984d851e7cd4276b8315b5f30943359d30cc
TharunKrishnaP <tharunkrishnap12@gmail.com>
2025-06-27 11:40:06 +0530

    commit

---
+++ Week1_PythonBasics/Karpekar_Constant.py
@@ -0,0 +1,14 @@
+def Karpekar_Constant(num,count=0):
+    num_str = f"{int(num):04d}"
+    asc_num = int("".join(sorted(num_str)))
+    desc_num = int("".join(sorted(num_str,reverse=True)))
+    diff = desc_num - asc_num
+    count += 1
+    if diff != 6174:
+        return Karpekar_Constant(diff,count)
+    return count
+
+print("Input must be a 4-digit number where a digit can repeat utmost 2 times")
+input_num = input("Enter number: ")
+count = Karpekar_Constant(input_num)
+print(f"Number {input_num} took {count} iterations to reach Karpekar Constant 6174")
