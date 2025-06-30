3c334a74e3306f56907faa416e819ed7a48674e7
TharunKrishnaP <tharunkrishnap12@gmail.com>
2025-06-30 11:27:13 +0530

    commit

---
+++ list1.py
@@ -0,0 +1,6 @@
+l1 = [1,43,6,10]
+l2 = [-5,43,1,9]
+print(l1>l2)
+print(l1<l2)
+print(l1==l2)
+print(l1!=l2)
---
+++ list2.py
@@ -0,0 +1,12 @@
+l1 = [1,2,3,4,5,6,7]
+
+#Different slicing methods but same output
+print(l1)
+print(l1[:])
+print(l1[::])
+print(l1[0::])
+print(l1[0::1])
+print(l1[::1])
+print(l1[0::1])
+print(l1[-10::])
+print(l1[:10:])
