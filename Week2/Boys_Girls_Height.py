dece7cd98794b45a65f023c1eec6733d542d8d34
TharunKrishnaP <tharunkrishnap12@gmail.com>
2025-07-01 13:43:17 +0530

    commit

--- Week2/Boys_Girls_Height.py
+++ Week2/Boys_Girls_Height.py
@@ -2,7 +2,7 @@
 t = int(input("Enter no. of test cases: "))
 def height_append(a,b):
     i = j = k = 0
-    while (i<len(a) or j<len(b)) and k<n:
+    while (i<len(a) or j<len(b)) and k<class_strength:
         if k==0:
             height_order.append(a[i])
             k += 1
@@ -21,22 +21,22 @@ def height_append(a,b):
                     height_order.append(b[j])
                     j += 1
             k +=1
-    if len(height_order) == n:
+    if len(height_order) == class_strength:
         print("Yes")
     else:
         print("No")
 
 for i in range(1,t+1):
     print(f"Test case {i}")
-    n = int(input("Class strength: "))
-    print(f" Boys = Girls = {n//2}")
-    b = list(map(int, input("Enter boys height : ").split()))
-    g = list(map(int, input("Enter girls height : ").split()))
-    b.sort()
-    g.sort()
+    class_strength = int(input("Class strength: "))
+    print(f" Boys = Girls = {class_strength//2}")
+    boys = list(map(int, input("Enter boys height : ").split()))
+    girls = list(map(int, input("Enter girls height : ").split()))
+    boys.sort()
+    girls.sort()
     height_order = []
-    if b[0] < g[0]:
-        height_append(b,g)
+    if boys[0] < girls[0]:
+        height_append(boys,girls)
     else:
-        height_append(g,b)
+        height_append(girls,boys)
     print(height_order)
